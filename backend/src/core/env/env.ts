ce6fa35c18c8615b773bf465791e6df70fd82850
Lucas Silva <lucassilva080397@gmail.com>
2023-09-15 10:13:23 -0300

    .

--- backend/src/core/env/env.ts
+++ backend/src/core/env/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
